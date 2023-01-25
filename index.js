18e10372a8e6dab34aa194730f7d0cfb461e7e2a
Shivam Agarwal <itisshivam22@gmail.com>
2023-01-25 14:39:41 +0530

    feat: create PATCH and PUT route for modifying tasks

--- index.js
+++ index.js
@@ -1,44 +1,57 @@
-const express = require('express')
-const app = express()
-const Todo = require('./controllers')
-const bodyParser = require('body-parser')
-const PORT = 3000
+const express = require('express');
+const app = express();
+const Todo = require('./controllers');
+const bodyParser = require('body-parser');
+const PORT = 3000;
 
 app.use(bodyParser.json());
 
 app.route('/tasks')
-    .get(async (req, res) => {
-        const tasks = await Todo.getTasks()
-        res.status(200)
-        res.json(tasks)
-    })
-    .post(async (req, res) => {
-        let taskData = req.body
-        try {
-            let task = await Todo.createTask(taskData)
-            res.status(201)
-            res.json(task)
-        }
-        catch {
-            res.status(400)
-            res.json({message: 'Input is not in JSON'})
-        }
-    })
+  .get(async (req, res) => {
+    const tasks = await Todo.getTasks();
+    res.status(200);
+    res.json(tasks);
+  })
+  .post(async (req, res) => {
+    let taskData = req.body;
+    try {
+      let task = await Todo.createTask(taskData);
+      res.status(201);
+      res.json(task);
+    }
+    catch {
+      res.status(400);
+      res.json({message: 'Input is not in JSON'});
+    }
+  });
 
 app.route('/tasks/:id')
-    .get(async (req, res) => {
-        try {
-            const {id} = req.params
-            const task = await Todo.getTask(id);
-            res.status(200)
-            res.json(task)
-        }
-        catch(error) {
-            res.status(404)
-            res.json({message: error})
-        }
-    })
+  .get(async (req, res) => {
+    try {
+      const {id} = req.params;
+      const task = await Todo.getTask(id);
+      res.status(200);
+      res.json(task);
+    }
+    catch(error) {
+      res.status(404);
+      res.json({message: error});
+    }
+  })
+  .patch(async (req, res) => {
+    const {id} = req.params;
+    const task = await Todo.completeTask(id);
+    res.status(200);
+    res.json(task);
+  })
+  .put(async (req, res) => {
+    let taskData = req.body;
+    const {id} = req.params;
+    let updatedTask = await Todo.updateTask(id, taskData);
+    res.status(200);
+    res.json(updatedTask);
+  });
 
 app.listen(PORT, () => {
-    console.log(`Serving on port ${PORT}`)
-})
+  console.log(`Serving on port ${PORT}`);
+});
