4910aba104506029cb1588532881eab893177424
Shivam Agarwal <itisshivam22@gmail.com>
2023-01-25 13:03:03 +0530

    chore: add sample todo tasks to work with

---
+++ data.js
@@ -0,0 +1,14 @@
+const taskList = [
+  {
+    id: 1,
+    name: 'Cooking Food',
+    isComplete: false
+  },
+  {
+    id: 2,
+    name: 'Going for a walk',
+    isComplete: false
+  }
+];
+
+module.exports = taskList;
